4368882d37eb34f5b55fc360b6013b057552c9af
louisgirard <louis.girard17@hotmail.com>
2020-08-13 19:00:35 +0200

    continue music when loading levels (dont destroy on load)

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,10 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
+    AudioSource musicPlayer;
+
+    private void Awake()
+    {
+        musicPlayer = GetComponent<AudioSource>();
+        DontDestroyOnLoad(musicPlayer);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
