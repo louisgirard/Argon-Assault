f6dd12e6dec952d0712dcbde852c27a12180a467
louisgirard <louis.girard17@hotmail.com>
2020-08-19 18:42:16 +0200

    add ship horizontal movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,9 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("m/s")][SerializeField] float xSpeed = 4f;
+    [Tooltip("m")] [SerializeField] float xRange = 4.5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +15,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        print(horizontalThrow);
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xSpeed * xThrow * Time.deltaTime;
+        float rawNewXPosition = transform.localPosition.x + xOffset;
+        float clampedXPosition = Mathf.Clamp(rawNewXPosition, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPosition, transform.localPosition.y, transform.localPosition.z);
+
+        print(xOffset);
     }
 }
