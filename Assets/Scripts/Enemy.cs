2220a07369d0284d1b54231e54c7118dc1b2af08
louisgirard <louis.girard17@hotmail.com>
2020-08-23 16:46:40 +0200

    add component (box colliders on enemy ships) at runtime to allow ourselves to reimport asset packs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         print("Ship " + gameObject.name + " collided with particles " + other.name);
